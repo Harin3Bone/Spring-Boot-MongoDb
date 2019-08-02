287060a272208b53530cfb4555a8f90084f1da15
Zchwazer <yamiyomuyumi@gmail.com>
2019-08-02 14:30:26 +0700

    Get Start
    create "Hello World" project

---
+++ src/main/java/th/co/practice/Library/Hello.java
@@ -0,0 +1,13 @@
+package th.co.practice.Library;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @RequestMapping("/")
+    public String index(){
+        return "Hello World";
+    }
+}
